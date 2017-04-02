42fc39f6f7c741fd12facdf85caa247da4a65c7b
alexlee <alexlee@175.159.87.162>
2017-04-02 16:32:02 +0800

    modified main controller

--- src/main/java/ee4216/group10/controllers/MainController.java
+++ src/main/java/ee4216/group10/controllers/MainController.java
@@ -3,6 +3,10 @@ package ee4216.group10.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+ *  Main Controller that maps Thymeleaf templates to URLs
+ */
+
 @Controller
 public class MainController {
 	
