d452fc227d8588e9519c9df9dc92a5bf28ce2543
alexlee <alexlee@175.159.87.162>
2017-04-02 16:02:27 +0800

    initial commit

---
+++ src/main/java/ee4216/group10/EvChargerLocatorApplication.java
@@ -0,0 +1,12 @@
+package ee4216.group10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvChargerLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvChargerLocatorApplication.class, args);
+	}
+}
---
+++ src/test/java/ee4216/group10/EvChargerLocatorApplicationTests.java
@@ -0,0 +1,16 @@
+package ee4216.group10;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvChargerLocatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
