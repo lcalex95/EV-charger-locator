d2073ef4c10f523dee8a20dceff49d55e6afd696
alexlee <alexlee@175.159.87.162>
2017-04-07 17:14:50 +0800

    modified index

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,15 +1,32 @@
-var app = angular.module('index', [])
-	.controller('indexCtrl', function($scope) {
+var app = angular.module('index', ['ui.grid'])
+	.controller('indexCtrl', function($scope, $http, $log) {
     // default map options
 
     var mapOptions = {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 22.3564,
+            lng: 114.1095
         },
-        zoom: 8
+        zoom: 11
     };
+    $scope.locations = [];
+    $scope.visibleLocations = [];
+    
 
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    function getAllLocations() {
+    	$http.get('/get-xml')
+    		.success(function(data) {
+    			if(data != undefined) {
+    				$scope.locations = data;
+    			}
+    			else {
+    				$log.log('ERROR: Unable to load locations data');
+    			}
+    		})
+    }
+    
+    getAllLocations();
 
 });
