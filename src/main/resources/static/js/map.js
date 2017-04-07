54ea60a0f55832e209a99051b88217a04fda3eb2
tszzzyeung <tszzzyeung@172.16.2.179>
2017-04-07 17:52:26 +0800

    updated map.js added loadlocation() 1. with multiple markers plotting 2. with infoWindow showing the types of charger

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -4,12 +4,49 @@ var app = angular.module('index', [])
 
     var mapOptions = {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 22.3564,
+            lng: 114.1095
         },
-        zoom: 8
+        zoom: 12
     };
-
+    var locations = [
+    	{
+    		latittude: 22.4504833221436,
+    		longtittude: 114.160835266113,
+    		chargeType: "SemiQuick"
+    	},
+    	{
+    		latittude: 22.4924488067627,
+    		longtittude: 114.13890838623,
+    		chargeType: "Standard"
+    	},
+    ];
+     
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
+      
+    function loadLocation(){
+  
+    var marker,i;
+    
+        
+    for(i=0;i<locations.length;i++){
+    	var contentString = locations[i].chargeType;
+    	var infowindow = new google.maps.InfoWindow({
+    	    content: contentString
+    	});
+    	marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i].latittude, locations[i].longtittude),
+        map: $scope.map
+          });
+    	
+    	marker.addListener('click', (function(marker,i) {
+    		return function(){
+    	
+        infowindow.open($scope.map, marker);
+    		}
+        })(marker,i));
+      }
+    }
+      
+    loadLocation();
 });
